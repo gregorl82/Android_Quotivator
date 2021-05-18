37a1662dc092dd3aa9a69e4b6beceb4c53ba5092
Gregor Lennie <gregorlennie@hotmail.co.uk>
2021-05-18 17:31:39 +0100

    Set up view binding

--- app/src/main/java/com/example/quotivator/MainActivity.kt
+++ app/src/main/java/com/example/quotivator/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.quotivator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.quotivator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.quotivateButton.setOnClickListener {
+            displayQuote()
+        }
+    }
+
+    private fun displayQuote() {
+        Toast.makeText(this, "Quotivate button clicked!", Toast.LENGTH_SHORT).show()
     }
 }
